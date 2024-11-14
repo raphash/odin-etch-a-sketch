532fc0f98f845f2c0d0fa254cd06cecaf149874b
raphash <175216103+raphash@users.noreply.github.com>
2024-11-14 20:29:29 -0300

    feat: Now can player choose the grid scale
    This feature allow the player to choose the grid
    container pixels width and height (scale).

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const grid = document.querySelector(".grid");
+const MAX_GRID_SIZE = 32;
+const MIN_GRID_SIZE = 1;
 
-function createGrid(scale) {
+const grid = document.querySelector(".grid");
+const customGridBtn = document.querySelector(".custom");
+
+function setGrid(scale) {
   const dimens = {
     width: grid.getBoundingClientRect().width / scale,
     height: grid.getBoundingClientRect().height / scale
@@ -9,13 +13,52 @@ function createGrid(scale) {
   for (let i = 0; i < scale; i++) {
     for (let j = 0; j < scale; j++) {
       const pixel = document.createElement("div");
+            pixel.setAttribute("draggable", "false");
+            pixel.classList.add("pixel");
             pixel.style.width = `${dimens.width}px`;
             pixel.style.height = `${dimens.height}px`;
-            pixel.classList.add("pixel");
   
       grid.appendChild(pixel);
     }
   }
 }
 
-createGrid(32);
+function setDraw() {
+  function randomHsl() {
+    return `hsla(${(Math.random() * 360)}, 25%, 50%, 1)`;
+  }
+
+  // fills the rect when mouse moves and left button is pressed.
+  grid.addEventListener("mousemove", (e)=>{
+    if (e.buttons == 1) {
+      e.target.style.outline = "none";
+      e.target.style.background = randomHsl();
+    }
+  })
+}
+
+function setCustomSize(size) {
+  let scale;
+
+  do {
+    scale = prompt(`Custom Scale: ${MIN_GRID_SIZE} ~ ${MAX_GRID_SIZE}`);
+    
+    // Clear all "pixels" in grid.
+    grid.textContent = "";
+
+    if (!scale) {
+      setGrid(32);
+      break;
+    }
+  } while (scale > MAX_GRID_SIZE || scale < MIN_GRID_SIZE);
+
+  return scale;
+}
+
+customGridBtn.addEventListener("click", ()=>{
+  const customSize = setCustomSize();
+  setGrid(customSize);
+});
+
+setGrid(10);
+setDraw();
